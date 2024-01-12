6a943d24dcf412b9f0bc4a0f38ea6ee42a33e088
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-01-12 17:41:31 +0100

    changed object cache to class Cache

--- utils/cache.js
+++ utils/cache.js
@@ -1,47 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-const clear = () => {
-    const folderPath = path.join(__dirname, '../cache');
+class Cache {
+    static {
+        this.clear = () => {
+            const folderPath = path.join(__dirname, '../cache');
+        
+            if (fs.existsSync(folderPath)) {
+                fs.rmSync(folderPath, { recursive: true, force: true }, err => {
+                    if (err) throw err;
+                });
+            }
+        
+            return true;
+        }
 
-    if (fs.existsSync(folderPath)) {
-        fs.rmSync(folderPath, { recursive: true, force: true }, err => {
-            if (err) throw err;
-        });
+        this.getFile = (filePath, fileType) => {
+            const absoluteFilePath = path.join(__dirname, `../cache/${fileType}`, filePath);
+        
+            if (!fs.existsSync(absoluteFilePath)) return false;
+            
+            const outputFile = fs.readFileSync(absoluteFilePath);
+        
+            return outputFile;
+        }
+
+        this.save = (filePath, fileContent, fileType) => {
+            const absoluteFilePath = path.join(__dirname, `../cache/${fileType}`, filePath);
+            const absoluteFolderPath = path.dirname(absoluteFilePath);
+        
+            if (!fs.existsSync(absoluteFolderPath)) {
+                fs.mkdirSync(absoluteFolderPath, {recursive: true});
+            }
+        
+            fs.writeFileSync(absoluteFilePath, fileContent, err => {
+                if (err) throw err;
+            });
+        
+            return true;
+        }
     }
-
-    return true;
 }
 
-const getFile = (filePath, fileType) => {
-    const absoluteFilePath = path.join(__dirname, `../cache/${fileType}`, filePath);
-
-    if (!fs.existsSync(absoluteFilePath)) return false;
-    
-    const outputFile = fs.readFileSync(absoluteFilePath);
-
-    return outputFile;
-}
-
-const save = (filePath, fileContent, fileType) => {
-    const absoluteFilePath = path.join(__dirname, `../cache/${fileType}`, filePath);
-    const absoluteFolderPath = path.dirname(absoluteFilePath);
-
-    if (!fs.existsSync(absoluteFolderPath)) {
-        fs.mkdirSync(absoluteFolderPath, {recursive: true});
-    }
-
-    fs.writeFileSync(absoluteFilePath, fileContent, err => {
-        if (err) throw err;
-    });
-
-    return true;
-}
-
-const cache = {
-    clear,
-    getFile,
-    save
-}
-
-module.exports = cache;
+module.exports = Cache;
